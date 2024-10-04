7b04eaad1e437a6f993d3db761a4df3f9471c9cc
zaydolas74 <r0934607@student.thomasmore.be>
2024-10-04 14:14:02 +0200

    edit

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 let messages = [
-  { user: "pikachu", message: "Wassup" },
-  { user: "Zayd", message: "Yoo hello" },
+  { id: 0, user: "pikachu", message: "Wassup" },
+  { id: 1, user: "Zayd", message: "Yoo hello" },
 ];
 
 //GET1
@@ -48,10 +48,8 @@ router.put("/:id", function (req, res) {
     status: "succes",
     message: "Message updated",
     data: {
-      message: {
-        user: "Pikachu",
-        text: "Hi, i'm an updated message",
-      },
+      user: "Pikachu",
+      text: "Hi, i'm an updated message",
     },
   });
 });
